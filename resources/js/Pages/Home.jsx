dae5f8fc70c7a05264bbe7ccc4905574996075d3
ZyroGlitch <j.buliag.530734@umindanao.edu.ph>
2026-03-16 14:31:03 +0800

    React & Inertia Added

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <h1>HELLO WORLD</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat est vitae quidem, voluptate dignissimos reiciendis dolor reprehenderit impedit adipisci libero! Debitis repellat enim eligendi illo, dolor aspernatur minima. Debitis, ducimus.</p>
+        </>
+    )
+}
